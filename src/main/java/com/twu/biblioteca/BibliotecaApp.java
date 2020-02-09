f5e4506d991aa974f8ae8f194ebeb76f7d5fce87
charles161 <fortloop.charles@gmail.com>
2020-02-09 09:34:32 +0530

    Requirement:1.2 - Add: To see a list of books after the welcome message

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -1,11 +1,30 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static final String GREETING_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
     public static void main(String[] args) {
         System.out.println(greeting());
+        System.out.println();
+        viewBookList();
+    }
+
+    private static void viewBookList() {
+        System.out.println("Books Available:");
+        System.out.println();
+        String book1 = "Old man and the sea";
+        String book2 = "To kill a mocking bird";
+        List<String> bookList = new ArrayList<>();
+        bookList.add(book1);
+        bookList.add(book2);
+        int serial = 0;
+        for (String book : bookList) {
+            System.out.println(++serial + ". " + book);
+        }
     }
 
     public static String greeting() {
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -2,14 +2,31 @@ package com.twu.biblioteca;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BibliotecaAppTest {
 
     @Test
     public void shouldDisplayGreetingMessageWhenTheApplicationStarts() {
         String expectedGreeting = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
         assertEquals(expectedGreeting, BibliotecaApp.greeting());
+    }
+
+    @Test
+    public void shouldDisplayListOfBooksAfterTheGreeting() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String expectedGreeting = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        String expectedBookList = "Books Available:\n\n1. Old man and the sea\n2. To kill a mocking bird\n";
+        String expectedOutput = expectedGreeting + "\n\n" + expectedBookList;
+
+        BibliotecaApp.main(new String[]{});
+
+        assertEquals(expectedOutput, outContent.toString());
     }
 
 }
