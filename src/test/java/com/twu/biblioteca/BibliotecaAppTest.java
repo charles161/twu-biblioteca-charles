b3d6649068ea126a4bc0715ed7797aaddc25635f
charles161 <fortloop.charles@gmail.com>
2020-02-12 10:49:00 +0530

    Requirement:1.12 - Refactor: Modified BibliotecaAppTest to support Input mocks

--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -5,12 +5,6 @@ import com.twu.biblioteca.Exceptions.BookNotAvailableException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 import static org.mockito.Mockito.*;
 
 class BibliotecaAppTest {
@@ -26,7 +20,16 @@ class BibliotecaAppTest {
 
     Printer printer = mock(Printer.class);
     Library library = mock(Library.class);
-    BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
+    Input input = mock(Input.class);
+    BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, input);
+
+    private void simulateBookNameInput(int userInput, String bookName) {
+        when(input.hasNextInt()).thenReturn(true, false);
+        when(input.hasNextLine()).thenReturn(true);
+        when(input.hasNext()).thenReturn(true);
+        when(input.nextLine()).thenReturn("\n", bookName);
+        when(input.nextInt()).thenReturn(userInput);
+    }
 
 
     @Test
@@ -48,10 +51,9 @@ class BibliotecaAppTest {
     @Test
     public void shouldBeAbleToViewTheListOfBooksAvailableAfterTypingOneInMenu() throws BookNotAvailableException {
         int userInput = 1;
-        InputStream inputStream = new ByteArrayInputStream(Integer.toString(userInput).getBytes());
+        when(input.hasNextInt()).thenReturn(true,false);
+        when(input.nextInt()).thenReturn(userInput);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printAvailableBooks(Mockito.anyString(), Mockito.anyString(), Mockito.anyList());
@@ -60,10 +62,9 @@ class BibliotecaAppTest {
     @Test
     public void shouldNotifyUserWhenAnInvalidOption6IsSelected() throws BookNotAvailableException {
         int userInput = 6;
-        InputStream inputStream = new ByteArrayInputStream(Integer.toString(userInput).getBytes());
+        when(input.hasNextInt()).thenReturn(true,false);
+        when(input.nextInt()).thenReturn(userInput);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printErrorMessage(ERROR_MESSAGE);
@@ -71,13 +72,12 @@ class BibliotecaAppTest {
 
 
     @Test
-    void shouldDisplayTheListOfBooksForAValidInputAfterAnInvalidInput() throws BookNotAvailableException {
+    void shouldDisplayTheListOfBooksForAValidInputAfterAnInvalidInput() {
         int invalidInput = 7;
         int validInput = 1;
-        InputStream inputStream1 = new ByteArrayInputStream((invalidInput + "\n" + validInput).getBytes());
+        when(input.hasNextInt()).thenReturn(true,true,false);
+        when(input.nextInt()).thenReturn(invalidInput,validInput);
 
-        System.setIn(inputStream1);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printErrorMessage(ERROR_MESSAGE);
@@ -86,72 +86,52 @@ class BibliotecaAppTest {
 
     @Test
     @ExpectSystemExit
-    void shouldExitTheApplicationWhen2IsTyped() throws BookNotAvailableException {
+    void shouldExitTheApplicationWhen2IsTyped() {
         int userInput = 2;
-        InputStream inputStream1 = new ByteArrayInputStream(Integer.toString(userInput).getBytes());
+        when(input.hasNextInt()).thenReturn(true,false);
+        when(input.nextInt()).thenReturn(userInput);
 
-        System.setIn(inputStream1);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(0)).printErrorMessage(ERROR_MESSAGE);
     }
 
     @Test
-    public void shouldDisplayTheEnterBookMessageWhenCheckoutOptionIsSelected() throws BookNotAvailableException {
+    public void shouldDisplayTheEnterBookMessageWhenCheckoutOptionIsSelected() {
         int userInput = 3;
-        InputStream inputStream = new ByteArrayInputStream(Integer.toString(userInput).getBytes());
+        when(input.hasNextInt()).thenReturn(true,false);
+        when(input.nextInt()).thenReturn(userInput);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printMessage(ENTER_BOOK_MESSAGE);
     }
 
     @Test
-    public void shouldNotDisplayTheNameOfTheBook1WhenCheckedOut() throws BookNotAvailableException {
+    public void shouldCheckoutABookWhenNameIsEntered() throws BookNotAvailableException {
         int userInput1 = 3;
         String bookName = "Old man and the sea";
         int userInput2 = 1;
-        InputStream inputStream = new ByteArrayInputStream((userInput1 + "\n" + bookName + "\n" + userInput2).getBytes());
-        Book book = new Book("To Kill A Mocking Bird", "Harper Collins", 2013);
-        List<Book> bookList = new ArrayList<>();
-        bookList.add(book);
-        List<String> bookDetails = Book.buildList(bookList);
-        when(library.availableBooksDetail()).thenReturn(bookDetails);
+        when(input.hasNextInt()).thenReturn(true,true,false);
+        when(input.hasNextLine()).thenReturn(true);
+        when(input.hasNext()).thenReturn(true);
+        when(input.nextLine()).thenReturn("\n",bookName);
+        when(input.nextInt()).thenReturn(userInput1,userInput2);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printMessage(ENTER_BOOK_MESSAGE);
-        verify(library, times(1)).checkout(bookName);
+        verify(library, times(1)).checkout(Mockito.anyString());
     }
 
-    @Test
-    public void shouldCheckoutABookWhenAbOOkNameIsEntered() throws BookNotAvailableException {
-        int userInput1 = 3;
-        String bookName = "To Kill A Mocking Bird";
-        InputStream inputStream = new ByteArrayInputStream((userInput1 + "\n" + bookName).getBytes());
-
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
-        bibliotecaApp.processUserInput();
-
-        verify(printer, times(1)).printMessage(ENTER_BOOK_MESSAGE);
-        verify(library, times(1)).checkout(bookName);
-    }
 
     @Test
     void shouldDisplaySuccessMessageAfterSuccessfulCheckout() throws BookNotAvailableException {
         int userInput1 = 3;
         String bookName = "To Kill A Mocking Bird";
-        InputStream inputStream = new ByteArrayInputStream((userInput1 + "\n" + bookName).getBytes());
+        simulateBookNameInput(userInput1, bookName);
         doNothing().when(library).checkout(bookName);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printMessage(SUCCESSFUL_CHECKOUT_MESSAGE);
@@ -161,11 +141,9 @@ class BibliotecaAppTest {
     void shouldDisplayErrorMessageAfterUnsuccessfulCheckout() throws BookNotAvailableException {
         int userInput1 = 3;
         String bookName = "To Kill A Mocking Bird";
-        InputStream inputStream = new ByteArrayInputStream((userInput1 + "\n" + bookName).getBytes());
+        simulateBookNameInput(userInput1, bookName);
         doThrow(BookNotAvailableException.class).when(library).checkout(bookName);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printMessage(UNSUCCESSFUL_CHECKOUT_MESSAGE);
@@ -175,25 +153,21 @@ class BibliotecaAppTest {
     void shouldBeAbleToReturnABookWhenABookNameIsEntered() throws BookNotAvailableException {
         int userInput1 = 4;
         String bookName = "To Kill A Mocking Bird";
-        InputStream inputStream = new ByteArrayInputStream((userInput1 + "\n" + bookName).getBytes());
+        simulateBookNameInput(userInput1, bookName);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printMessage(ENTER_BOOK_MESSAGE);
-        verify(library, times(1)).returnBook(bookName);
+        verify(library, times(1)).returnBook(Mockito.anyString());
     }
 
     @Test
     void shouldDisplaySuccessMessageAfterSuccessfulReturn() throws BookNotAvailableException {
         int userInput1 = 4;
         String bookName = "To Kill A Mocking Bird";
-        InputStream inputStream = new ByteArrayInputStream((userInput1 + "\n" + bookName).getBytes());
+        simulateBookNameInput(userInput1, bookName);
         doNothing().when(library).returnBook(bookName);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printMessage(SUCCESSFUL_RETURN_MESSAGE);
@@ -203,13 +177,13 @@ class BibliotecaAppTest {
     void shouldDisplayErrorMessageAfterUnsuccessfulReturn() throws BookNotAvailableException {
         int userInput1 = 4;
         String bookName = "To Kill A Mocking Bird";
-        InputStream inputStream = new ByteArrayInputStream((userInput1 + "\n" + bookName).getBytes());
+        simulateBookNameInput(userInput1, bookName);
         doThrow(BookNotAvailableException.class).when(library).returnBook(bookName);
 
-        System.setIn(inputStream);
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(printer, library, new InputWrapper(new Scanner(System.in)));
         bibliotecaApp.processUserInput();
 
         verify(printer, times(1)).printMessage(UNSUCCESSFUL_RETURN_MESSAGE);
     }
+
+
 }
